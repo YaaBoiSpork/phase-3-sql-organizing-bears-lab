60fa161a1f521c80e87cbe9dac411cdfc4fd4c20
Stephen <stephen.a.nerby@gmail.com>
2023-01-01 20:29:01 -0500

    commit tree ec914dd131c645a537fcc5e0da94c741f20a5d4c

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 5, "M", "Brown", "Mellow", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 2, "M", "Black", "Energetic", 1);
+INSERT INTO bears VALUES (3, "Tabitha", 14, "F", "Grey", "Lazy", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 8, "M", "Brown", "Dominant", 0);
+INSERT INTO bears VALUES (5, "Melissa", 9, "F", "Yellow", "Caring", 1);
+INSERT INTO bears VALUES (6, "Grinch", 12, "M", "Green", "Grouchy", 1);
+INSERT INTO bears VALUES (7, "Wendy", 7, "F", "Red", "Annoying", 0);
+INSERT INTO bears VALUES (8, null, 12, "M", "White", null, 0);
